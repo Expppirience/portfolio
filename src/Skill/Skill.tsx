2eb9d6698f62789d36afe2b91373b8cbdc45a8cf
a1tsv <forakk01@yandex.ru>
2022-11-26 23:48:22 +0300

    skills block added

--- src/Skill/Skill.tsx
+++ src/Skill/Skill.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Skill.css'
+import "./Skill.css";
 
 export const Skill = () => {
   return (
@@ -9,7 +9,12 @@ export const Skill = () => {
         <div className="item-skill__img-placeholder"></div>
       </div>
       <h2 className="item-skill__title">React</h2>
-      <div className="item-skill__description">Skill description</div>
+      <div className="item-skill__description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quod
+        incidunt quia sapiente voluptatem accusantium amet aut? Autem placeat
+        dolorum eos optio omnis excepturi rem officia repellat, reiciendis
+        architecto assumenda.
+      </div>
     </article>
   );
 };
